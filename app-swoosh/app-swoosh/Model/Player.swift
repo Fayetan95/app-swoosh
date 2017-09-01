42afaa4371d096067aa64b9281d274ad0265cb8b
Fayetan95 <fayetan95@gmail.com>
2017-09-01 23:36:12 +0800

    swoosh - lesson-09

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Tan Juat Fwee on 1/9/17.
+//  Copyright © 2017 Faye Tan. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
